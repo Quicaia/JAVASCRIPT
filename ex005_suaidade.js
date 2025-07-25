d23ac5830ea057de6aaef246911e38ab87db4504
Manuel Quicaia <155465120+Quicaia@users.noreply.github.com>
2025-07-25 21:49:09 +0100

    Create ex005_suaidade.js
    su

---
+++ ex005_suaidade.js
@@ -0,0 +1,18 @@
+//Você esta fazendo aniversario, mas n sabe quantas velas colocar no bolo. Sabe apenas o ano do seu nascimento e o ano atual.
+//Desenvolva um programa que calcule quantas velas colocar no bolo.
+
+const input = require('readline-sync');
+let anoAtual = new Date().getFullYear();
+let anoNascimento = Number(input.question('Digite o ano do seu nascimento: '));
+let idade = anoAtual - anoNascimento;
+
+if(anoNascimento > anoAtual) {
+    console.log('Ano de nascimento inválido. Por favor, insira um ano válido.');
+}
+else if (conditions) {
+    console.log(`Você deve colocar ${idade} vela(s) no bolo.`);
+    
+}else {
+    console.log('Erro ao calcular a idade. Verifique os dados inseridos.');
+}
+
